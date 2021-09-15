2a8cfb8e0717a5ecce3b832abbf3a7d2a783cb85
Iván <ivanamat@users.noreply.github.com>
2021-09-15 21:02:38 +0200

    ignore funcition

--- src/main.ts
+++ src/main.ts
@@ -298,6 +298,7 @@ async function run() {
     ) {
       core.debug('ENTRAR ENTRA EN EL IF')
 
+      /*
       token = await getAppToken(
         settings.owner,
         settings.appId,
@@ -305,9 +306,73 @@ async function run() {
         settings.clientId,
         settings.clientSecret
       )
+      */
 
-      core.debug('AHORA VIENE EL TOKEN:' + token)
+      // core.debug('AHORA VIENE EL TOKEN:' + token)
       // res.then(e => core.debug('MY TIKTOK IS:' + e))
+
+      // Create octokit instance as app
+      const appOctokit = github.getOctokit({
+        authStrategy: createAppAuth,
+        auth: {
+          appId: appId,
+          privateKey: privateKey
+        }
+      })
+
+      core.debug('HOLA, PUEDO CREAR EL OKTIKIT')
+
+      // Retrieve app installations list
+      const response = await appOctokit.request('GET /app/installations')
+      const data = response.data
+
+      core.debug(data)
+
+      let installationId = Number(0)
+
+      // Find app installationId by organization
+      for (let i = 0; i < data.length; i++) {
+        core.debug(`Installation: ${inspect(data[i])}`)
+        if (data[i]?.account?.login === organization) {
+          installationId = data[i].id
+          break
+        }
+      }
+
+      core.debug(`Installation ID: ${inspect(installationId)}`)
+      if (installationId === 0) {
+        throw new Error(
+          'The ' +
+            organization +
+            ' organization has no privileges to access this app. Please, check your credentials and the organization permissions.'
+        )
+      }
+
+      // Create app authentication
+      const auth = createAppAuth({
+        appId: appId,
+        privateKey: privateKey,
+        clientId: clientId,
+        clientSecret: clientSecret
+      })
+
+      // Authenticate as app installation and retrieve access token
+      const installationAuthentication = await auth({
+        type: 'installation',
+        installationId: installationId
+      })
+
+      // Set access token
+      // token = installationAuthentication.token
+      core.debug(installationAuthentication.token)
+      token = installationAuthentication.token
+
+      // Throw error of invalid credentials if token is empty ( or not found ).
+      if (token === '') {
+        throw new Error(
+          'Invalid credentials! You must provide a valid personal access token or valid Application Credentials. Application Credentials requires appId, privateKey, clientId, clientSecret, and installation. Please, review your defined credentials.'
+        )
+      }
     }
 
     // This sould be removed
