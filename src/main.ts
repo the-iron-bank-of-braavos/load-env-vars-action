d2e248403916fbaa85f69816ab2d1a5a2d4aa358
Iván <ivanamat@users.noreply.github.com>
2021-09-15 09:20:23 +0200

    remove async from getAppTplem

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ const path = require('path')
 const dotenv = require('dotenv')
 const {v4: uuidv4} = require('uuid')
 
-const getAppToken = async (
+const getAppToken = (
   organization,
   appId,
   privateKey,
