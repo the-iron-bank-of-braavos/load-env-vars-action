5b609fdd9ad3cf225fc72ba61bad05eadce06e2a
Iván <ivanamat@users.noreply.github.com>
2021-09-15 12:22:28 +0200

    await

--- src/main.ts
+++ src/main.ts
@@ -285,7 +285,7 @@ async function run() {
     ) {
       core.debug('ENTRAR ENTRA EN EL IF')
 
-      token = await getAppToken(
+      token = getAppToken(
         settings.owner,
         settings.appId,
         settings.privateKey,
