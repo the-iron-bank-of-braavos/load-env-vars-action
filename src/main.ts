3a8746fd61bdeb7b57b9915985bdcd0185f75a8a
Iván <ivanamat@users.noreply.github.com>
2021-09-16 14:43:14 +0200

    ArrayBuffer

--- src/main.ts
+++ src/main.ts
@@ -114,8 +114,7 @@ const cloneDotenvConfig = async (owner, repo, branch, token, destination) => {
     `archive-${repo}-${downloadUuid}${archiveExt}`
   )
   core.info(`Writing archive file [${archiveFilepath}] to disk`)
-  let archiveData = []
-  archiveData = Buffer.from(response.data, 'binary')
+  const archiveData: ArrayBuffer = Buffer.from(response.data, 'binary')
   await fs.promises.writeFile(archiveFilepath, archiveData)
 
   // Extract archive
