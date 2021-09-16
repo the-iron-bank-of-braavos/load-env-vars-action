a11ce87522832784fa0c4fbb41ad71e8240c75f2
Iván <ivanamat@users.noreply.github.com>
2021-09-16 13:15:06 +0200

    if

--- src/main.ts
+++ src/main.ts
@@ -101,7 +101,7 @@ const cloneDotenvConfig = async (owner, repo, branch, token, destination) => {
   core.info('Downloading zip archive')
   // core.debug(params)
   const response = await downloadRepo(params)
-  if (response.status !== 200) {
+  if (!response.status == 200) {
     throw new Error(
       `Enable to fetch repository. HTTP:[${response.status}], content:[${response.data}]`
     )
